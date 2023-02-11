abb414528f6afc66337757f7b73640f4ce439383
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-02-11 13:12:57 -0500

    working build... not working arcade

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
 {
-  stdenv,
   python310Packages,
-  python310,
   fetchFromGitHub,
+  fetchzip,
+  unstablePymunk,
+  unstablePillow,
+  libGL,
   ...
 }: let
-  pytiled_parser = buildPythonPackage {
+  pytiled_parser = python310Packages.buildPythonPackage {
     name = "pytiled_parser";
     propagatedBuildInputs = with python310Packages; [
       typing-extensions
@@ -15,30 +17,35 @@
       repo = "pytiled_parser";
       owner = "pythonarcade";
       rev = "v2.2.0";
-      sha256 = "";
+      sha256 = "sha256-28OJI9dVz14/FRI+2E51ZLs13FsQdSh3tQ5Irz24HAs=";
     };
   };
-in
-  stdenv.mkDerivation {
-    name = "mazegame";
-    src = ./.;
+  myPyglet = python310Packages.buildPythonPackage rec {
+    pname = "pyglet";
+    version = "2.0.dev23";
+    src = fetchzip {
+      url = "https://files.pythonhosted.org/packages/dc/25/865361d60671e79e3e4f1fc7846750fd88fd7dd3c59fb8c410eda3f14a07/pyglet-2.0.dev23.zip";
+      sha256 = "sha256-QeQnmMdzeeLv8EsK6J10ihJ1k3jpckjMImOrHPclQxo=";
+    };
 
-    buildInputs = [
-      python310
-      (python310Packages.buildPythonPackage {
-        name = "arcade";
-        propagatedBuildInputs = with python310Packages; [
-          pillow
-          pyglet
-          pymunk
-          pytiled_parser
-        ];
-        src = fetchFromGitHub {
-          repo = "arcade";
-          owner = "pythonarcade";
-          rev = "2.6.17";
-          sha256 = "";
-        };
-      })
-    ];
-  }
+    doCheck = false;
+  };
+in (python310Packages.buildPythonPackage {
+  name = "arcade";
+  nativeBuildInputs = [
+    libGL
+  ];
+  propagatedBuildInputs = with python310Packages; [
+    unstablePillow
+    unstablePymunk
+    pytiled_parser
+    myPyglet
+  ];
+  src = fetchFromGitHub {
+    repo = "arcade";
+    owner = "pythonarcade";
+    rev = "2.6.17";
+    sha256 = "sha256-Et1zOnjNoHajshhDajAG3vVunQmzQ2tWgGp+m+4Rkdc=";
+  };
+  doCheck = false;
+})
--- flake.nix
+++ flake.nix
@@ -1,21 +1,46 @@
 {
   description = "Devshell with working arcade pypi package";
 
-  inputs.nixpkgs.url = github:nixos/nixpkgs?ref=22.11;
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs?ref=22.11;
+    nixpkgs-unstable.url = github:nixos/nixpkgs?ref=nixos-unstable;
+  };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
   }: let
     supportedSystems = [
       "x86_64-linux"
     ];
     genSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = genSystems (system: import nixpkgs {inherit system;});
+    unstable = genSystems (system: import nixpkgs-unstable {inherit system;});
+    pkgs = genSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          (_: super: {
+            unstablePymunk = unstable.${super.system}.python310Packages.pymunk;
+            unstablePillow = unstable.${super.system}.python310Packages.pillow;
+          })
+        ];
+      });
   in {
     packages = genSystems (system: {
-      mazegame = pkgs.${system}.callPackage ./default.nix {};
-      default = self.packages.${system}.mazegame;
+      arcade = pkgs.${system}.callPackage ./default.nix {};
+      default = self.packages.${system}.arcade;
     });
+
+    devShell = genSystems (system:
+      pkgs.${system}.mkShell {
+        packages = with pkgs.${system}; [
+          (python310.withPackages
+            (ps:
+              with ps; [
+                self.packages.${system}.arcade
+              ]))
+        ];
+      });
   };
 }
